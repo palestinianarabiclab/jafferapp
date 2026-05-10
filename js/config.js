88a410a04f70725240a3b4740c348867af9c3e0a
farouqmurtaja96 <farouqmurtaja96@gmail.com>
2026-05-10 18:50:46 +0300

    Refactor app configuration to use default settings and remove runtime config warning

--- js/config.js
+++ js/config.js
@@ -1,4 +1,28 @@
-const appConfig = window.__APP_CONFIG__ || {};
+const defaultAppConfig = {
+    firebase: {
+        apiKey: "AIzaSyC0haaF1Y9I-cbDfaHhSYD38COP_sMLnNI",
+        authDomain: "palestinian-arabic-lab.firebaseapp.com",
+        projectId: "palestinian-arabic-lab",
+        storageBucket: "palestinian-arabic-lab.firebasestorage.app",
+        messagingSenderId: "867160813546",
+        appId: "1:867160813546:web:fe81642287dff700ab4c93",
+        measurementId: "G-Q2GHE6545F",
+    },
+    googleCalendar: {
+        clientId: "728875114917-im3ui9lcb471mc43h11bgoq5fbr9kvu2.apps.googleusercontent.com",
+        apiKey: "AIzaSyCVff8yPsjylbM5Fhwl2HnqLyIpQhlbneE",
+        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+        scopes: "https://www.googleapis.com/auth/calendar.events",
+        redirectUri: window.location.origin + "/",
+    },
+    emailjs: {
+        publicKey: "",
+        serviceId: "",
+        templateId: "",
+    },
+};
+
+const appConfig = window.__APP_CONFIG__ || defaultAppConfig;
 const firebaseConfig = appConfig.firebase || {};
 const hasFirebaseConfig = Boolean(
     firebaseConfig.apiKey &&
@@ -26,9 +50,3 @@ window.emailJsConfig = {
     serviceId: appConfig.emailjs?.serviceId || "",
     templateId: appConfig.emailjs?.templateId || "",
 };
-
-if (!window.__APP_CONFIG__) {
-    console.warn(
-        "Runtime app config is missing. Create js/config.runtime.js locally or inject it during deployment."
-    );
-}
